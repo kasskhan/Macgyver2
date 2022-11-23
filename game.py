02d2488f5474a44354b9acaff80e9abc5112348f
Kassane <kasskhan1ere@gmail.com>
2022-11-23 15:20:58 -0300

    Encore

--- game.py
+++ game.py
@@ -2,10 +2,18 @@ import pygame
 
 pygame.init() #initialisation
 size = (800, 800) #Taille de l'écran
-pygame.display.set_mode(size)
+win = pygame.display.set_mode(size)
 pygame.display.set_caption('Jeu Macgiver') #Titre de la fenêtre
+framework = pygame.image.load('/image/real_background.jpg')
 
 state = True #Lancer l'application
 
 while state:
+
+    win.blit(framework(0, 0))
+
     pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            state = False
