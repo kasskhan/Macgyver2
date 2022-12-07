f471492a320f6e8ab8336170e747f76df52aedba
Kassane <kasskhan1ere@gmail.com>
2022-12-07 14:19:40 -0300

    Hello

--- game.py
+++ game.py
@@ -1,17 +1,21 @@
 import pygame
 
 pygame.init() #initialisation
-size = (800, 800) #Taille de l'écran
+size = (563, 375) #Taille de l'écran
 win = pygame.display.set_mode(size)
 pygame.display.set_caption('Jeu Macgiver') #Titre de la fenêtre
-framework = pygame.image.load('/image/real_background.jpg')
-
+framework = pygame.image.load('image/real_background.jpg')
+png = pygame.image.load('image/walk_cycle.gif')
 state = True #Lancer l'application
+x, y = 0, 0
 
 while state:
 
-    win.blit(framework(0, 0))
+    win.blit(framework, (0, 0))
+    win.blit(png, (x, y))
 
+    pygame.time.Clock().tick(30)
+    x += 20
     pygame.display.update()
 
     for event in pygame.event.get():
