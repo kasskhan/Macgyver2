2837ad282cd661a928d6b5e4578e44427c77823e
Kassane <kasskhan1ere@gmail.com>
2022-12-11 20:12:21 -0300

    hello

--- game.py
+++ game.py
@@ -20,29 +20,49 @@ x, y = 0, 0
 
 while state:
 
+# Position des éléments
     win.blit(framework, (0, 0))
     win.blit(png, (x, y))
     win.blit(object, (430, 300))
 
-
+# Réinitialiser la fenètre
     pygame.display.update()
 
+# Fermer la fenêtre
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             state = False
-        
+
+#Déplacer le personnage
     if pygame.key.get_pressed()[pygame.K_RIGHT]:
         pygame.time.Clock().tick(30)
-        x += 5
+        if x == 75 and 15 < y < 100:
+            x == 75
+        elif x == 370 and -18 < y < 25:
+            x = 370
+        else:
+            x += 5
 
     elif pygame.key.get_pressed()[pygame.K_LEFT]:
         pygame.time.Clock().tick(30)
-        x -= 5
+        if x == -40:
+            x = -40
+        else:
+            x -= 5
 
     elif pygame.key.get_pressed()[pygame.K_DOWN]:
         pygame.time.Clock().tick(30)
-        y += 5
-
+        if -45 < x < 80 and y == 95:
+            y = 95
+        elif 100 < x < 230 and y == 20:
+            y = 20
+        elif 256 < x < 375 and y == 20:
+            y = 20
+        else:
+            y += 5
     elif pygame.key.get_pressed()[pygame.K_UP]:
         pygame.time.Clock().tick(30)
-        y -= 5
+        if y == -15:
+            y = -15
+        else:
+            y -= 5
